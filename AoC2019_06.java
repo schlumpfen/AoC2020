9cccc86ad0ced2e7304438a8ad8666052a72fa26
Stefan Roschewitz <5253061-schlumpfen@users.noreply.gitlab.com>
2020-12-09 13:27:24 +0100

    Tag 6 fertig

--- AoC2019_06.java
+++ AoC2019_06.java
@@ -18,7 +18,9 @@ public class AoC2019_06
         // Datei holen und Zeilenweise speichern
         ReadFile a =new ReadFile("input06.txt");
         list = a.gibArray();
-        Map<String, String> map = new HashMap<>();
+        
+        // silver
+        Map<String, Integer> map = new HashMap<>();
         map.clear();
         long calc =0;
         for (int y = 0; y < list.size(); y++)
@@ -26,25 +28,78 @@ public class AoC2019_06
 
             String zeile = list.get(y);
             if (zeile.length()>0){
-                System.out.println(zeile);
+                // System.out.println(zeile);
                 for(int i =0;i<zeile.length();i++){
                     
                     if(!map.containsKey(zeile.charAt(i))){
-                        map.put(String.valueOf(zeile.charAt(i)), String.valueOf(zeile.charAt(i)));
+                        map.put(String.valueOf(zeile.charAt(i)), 1);
                     };
                 }
             }
             else{
-                            System.out.println(map.size() +  " -----");
+                            // System.out.println(map.size() +  " -----");
                             calc += map.size();
                             map.clear();
             }
             
         }
-        System.out.println(map.size() +  " -----");
+        //        System.out.println(map.size() +  " -----");
          
         calc += map.size();
-        System.out.println(calc +  " berechnet.");
+        System.out.println("silber " + calc +  " berechnet.");
+        
+        // gold
+        map.clear();
+        calc =0;
+        int zeilenzaehler = 0;
+        for (int y = 0; y < list.size(); y++)
+        {
+            
+            String zeile = list.get(y);
+            if (zeile.length()>0){
+                // System.out.println(zeile);
+                for(int i =0;i<zeile.length();i++){
+                    
+                    if(!map.containsKey(String.valueOf(zeile.charAt(i)))){
+                        map.put(String.valueOf(zeile.charAt(i)), 1);
+                    }
+                    else {
+                        int temp = map.get(String.valueOf(zeile.charAt(i)));
+                        temp++;
+                        map.put(String.valueOf(zeile.charAt(i)), temp);
+                    }
+                }
+                zeilenzaehler++;
+            }
+            else{
+                            // System.out.println(map.size() +  " -----");
+                            // System.out.println(zeilenzaehler +  " -----");
+                            // Wenn Zeihlenzaehler = der Antwort anzahl dann haben alle yes gesagt zu der Frage.
+                            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                                if((entry.getValue()==zeilenzaehler)){
+                                   // System.out.println(entry.getKey() +  " ----- " + entry.getValue());
+                                   calc++;
+                                }
+                            }
+                            // calc += map.size();
+                            map.clear();
+                            zeilenzaehler = 0;
+                     }
+        }             
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if((entry.getValue()==zeilenzaehler)){
+               // System.out.println(entry.getKey() +  " ----- " + entry.getValue());
+               calc++;
+            }
+        }
+                    // calc += map.size();
+                            map.clear();
+                            zeilenzaehler = 0;
+            
+        
+        //        System.out.println(map.size() +  " -----");
+
+        System.out.println("gold " + calc +  " berechnet.");
     }
 }
          
